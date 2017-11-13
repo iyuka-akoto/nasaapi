f62f8d368420ca94c0876ffa693b5fcbe64430fa
Iyuka <iyuka@NPC-AIR2017.local>
2017-11-14 00:09:58 +0900

    add .gitignore

---
+++ 20171113-NasaApi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  20171113-NasaApi
+//
+//  Created by Iyuka on 2017/11/14.
+//  Copyright © 2017年 Iyuka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
