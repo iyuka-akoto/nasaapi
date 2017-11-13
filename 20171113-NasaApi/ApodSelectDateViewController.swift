7ba7869160de2768a952f8f0ee65720eb5c681c4
Iyuka <iyuka@NPC-AIR2017.local>
2017-11-14 00:22:40 +0900

    APODの2つのViewController用のSwiftファイルを作成

---
+++ 20171113-NasaApi/ApodSelectDateViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ApodSelectDateViewController.swift
+//  20171113-NasaApi
+//
+//  Created by Iyuka on 2017/11/14.
+//  Copyright © 2017年 Iyuka. All rights reserved.
+//
+
+import UIKit
+
+class ApodSelectDateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
