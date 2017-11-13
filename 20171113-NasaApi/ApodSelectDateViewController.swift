1132883767f569ec261a474f186365336f10b810
Iyuka <iyuka@NPC-AIR2017.local>
2017-11-14 00:35:30 +0900

    DataPickerから日付を取得して、YYYY-MM-DDへ整形して、APODShowResultVCへ日付を渡せた。

--- 20171113-NasaApi/ApodSelectDateViewController.swift
+++ 20171113-NasaApi/ApodSelectDateViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ApodSelectDateViewController: UIViewController {
 
+    @IBOutlet weak var selectDate: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,14 +24,22 @@ class ApodSelectDateViewController: UIViewController {
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        
+        // 日付フォーマット指定
+        let selectedDate: DateFormatter = DateFormatter()
+        selectedDate.dateFormat = "yyyy-MM-dd"
+        
+        // ApodShowResultViewControllerの日付代入変数へ渡す
+        let apodShowResultVC = segue.destination as! ApodShowResultViewController
+        apodShowResultVC.selectedDate = selectedDate.string(from: selectDate.date)
     }
-    */
+    
 
 }
--- 20171113-NasaApi/ApodShowResultViewController.swift
+++ 20171113-NasaApi/ApodShowResultViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ApodShowResultViewController: UIViewController {
 
+    var selectedDate: String = String()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
